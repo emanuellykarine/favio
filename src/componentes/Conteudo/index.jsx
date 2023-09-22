7613e847256f0c8a417f30864a3bbe477c9bc923
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2023-09-22 09:34:10 -0300

    aula (22-09)

--- src/componentes/Conteudo/index.jsx
+++ src/componentes/Conteudo/index.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Conteudo.module.css'
 
-const index = () => {
+const Conteudo = () => {
+
+const [nome_site, setNomeSite]=useState('');
+const [url, setUrl]=useState('')
+
+let favoritos=[]
+
+
+  function salvarFavorito(){
+    console.log('favoritos>>>>>>>>', favoritos)
+    favoritos=[...favoritos, {nome_site, url}]
+    localStorage.setItem("favorito",JSON.stringify({favoritos}))
+  }
+
   return (
-    <div>
-        Conteudo 
-        <form onSubmit={()=>alert("Favorito salvo")}>
-            <label> Nome do site</label>
-            <input name='nome-site'/>
-
-            <label >url</label>
-
-            
-        </form>
+    <div className={styles.area}>
+        Conteudo
+    <form className={styles.formulario} onSubmit={()=>alert("Favorito salvo")}>
+        <label>
+            Nome do Site
+        </label> 
+        <h1>{nome_site}</h1>
+        <input name='nome_site' onChange={(evento)=> {setNomeSite(evento.target.value); console.log(nome_site);}}></input> <br/>
+        <label>
+            URL
+        </label>
+        <h1>{url}</h1>
+        <input type='url' name='url' onChange={(evento)=> {setUrl(evento.target.value); console.log(url);}}></input>  <br/>
+        <button onClick={()=>salvarFavorito()}>Salvar</button>
+    </form>
     </div>
   )
 }
 
-export default index
+export default Conteudo;
